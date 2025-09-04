884e69da82b8ad60b6f225b06b0c792829409bcd
pywave <jumayevn@list.ru>
2025-09-04 09:59:22 +0500

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,9 +10,9 @@ export default function Header({ onSearch }) {
   };
 
   return (
-    <header className="bg-blue-600 sticky top-0 z-50 shadow-md">
-      <div className="max-w-6xl mx-auto flex items-center justify-between px-4 py-3 pt-24">
-        {/* Лого (текстом) */}
+    <header className="bg-blue-600 sticky top-0 z-50 shadow-md pt-safe">
+      <div className="max-w-6xl mx-auto flex items-center justify-between px-4 py-3">
+        {/* Лого */}
         <div className="flex items-center gap-2">
           <span className="text-white font-bold text-xl tracking-wide">
             ansormed
