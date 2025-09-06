38bae2487193d5e80bd082ec711c30183abe40ad
pywave <jumayevn@list.ru>
2025-09-07 03:26:01 +0500

    update

--- src/components/AdsCarousel.jsx
+++ src/components/AdsCarousel.jsx
@@ -47,7 +47,7 @@ export default function AdsCarousel() {
               <img
                 src={ad.image}
                 alt=""
-                className="w-full h-[70px] object-cover"
+                className="w-full h-[90px] object-cover"
               />
             </div>
           </SwiperSlide>
