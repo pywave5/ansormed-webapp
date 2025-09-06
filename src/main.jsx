acb51a23fae676d61efd412cd1921ba13fb719a1
pywave <jumayevn@list.ru>
2025-09-06 08:29:47 +0500

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { CartProvider } from "./context/CartContext";
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CartProvider>
+    <QueryClientProvider client={queryClient}>
       <App />
-    </CartProvider>
+    </QueryClientProvider>
   </StrictMode>
-)
+);
