a449883cedc21d3eaabf8466ec16eda4792dde47
pywave <jumayevn@list.ru>
2025-09-09 13:32:59 +0500

    update

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -84,7 +84,7 @@ export default function Products({ selectedId, onCategoryChange }) {
   const currentCategory = categories.find((c) => c.id === selectedId);
 
   return (
-    <div сlassName="pt-24">
+    <div className="pt-24">
       {/* название текущей категории */}
       {currentCategory && (
         <h1 className="text-lg font-semibold mb-3 mb-4">
